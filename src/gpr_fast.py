b2f3a28f6f38f6113541f6762c82ca95faaae4c3
Fede Raimondo <f.raimondo@fz-juelich.de>
2023-11-10 17:40:12 +0100

    Source files, just in case

---
+++ src/gpr_fast.py
@@ -0,0 +1,97 @@
+# %%
+import numpy as np
+
+from typing import Any, Dict, List, Optional, Union
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.model_selection import KFold, RepeatedKFold
+from sklearn.base import clone, RegressorMixin, BaseEstimator
+from sklearn.gaussian_process.kernels import (
+    RBF,
+    ConstantKernel,
+    WhiteKernel,
+    Matern,
+    RationalQuadratic,
+    ExpSineSquared,
+    DotProduct,
+    PairwiseKernel,
+)
+
+from julearn.utils import logger
+
+
+class fastGaussianProcessRegressor(RegressorMixin, BaseEstimator):
+    """
+    Gaussian process regression (GPR) divide and conquer algorithm
+    Given K fit K models using one fold at a time
+    The final prediction is the average of prediction of all K models
+    """
+
+    def __init__(self, n_splits=5, n_repeats=1, **kwargs) -> None:
+        self.n_splits = n_splits
+        self.n_repeats = n_repeats
+        self.gpr_model = GaussianProcessRegressor(**kwargs)
+
+    def fit(self, X, y):
+        X_ = X
+        if not isinstance(X, np.ndarray):
+            X_ = np.array(X)
+        y_ = y
+        if not isinstance(y, np.ndarray):
+            y_ = np.array(y)
+        if self.n_repeats > 1:
+            folds = RepeatedKFold(
+                n_splits=self.n_splits, n_repeats=self.n_repeats)
+        else:
+            folds = KFold(n_splits=self.n_splits)
+        logger.info(f"Using {folds}")
+        # bins = # partition into bins the y
+        # folds = StratifiedKFold(n_splits=self.n_splits)
+        # Use folds.split(X, bins)
+        models = []
+        for _, test_index in folds.split(X):
+            t_model = clone(self.gpr_model)
+            t_model.fit(X_[test_index], y_[test_index])
+            models.append(t_model)
+        logger.info(f"Fitted {len(models)}")
+        self.models = models
+
+    def predict(self, X):
+        predictions = []
+        for model in self.models:
+            predictions.append(model.predict(X))
+        return np.mean(predictions, axis=0)
+
+    def get_params(self, deep=True):
+        out = dict(
+            **self.gpr_model.get_params(deep=deep),
+            n_splits=self.n_splits,
+            n_repeats=self.n_repeats,
+        )
+        return out
+
+    def set_params(self, **kwargs):
+        """Set the parameters of this estimator.
+
+        The method works on simple estimators as well as on nested objects
+        (such as :class:`sklearn.pipeline.Pipeline`). The latter have
+        parameters of the form ``<component>__<parameter>`` so that it's
+        possible to update each component of a nested object.
+
+        Parameters
+        ----------
+        **params : dict
+            Estimator parameters.
+
+        Returns
+        -------
+        self : estimator instance
+            Estimator instance.
+        """
+        gpr_params = list(self.gpr_model.get_params(True).keys())
+
+        for param, val in kwargs.items():
+            if param in gpr_params:
+                self.gpr_model.set_params(**{param: val})
+            else:
+                setattr(self, param, val)
+        return self
---
+++ tests/3_plot.py
@@ -0,0 +1,179 @@
+# %%
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# %% Config visuals
+sns.set_theme(style="whitegrid")
+sns.set_context("paper", font_scale=1.5, rc={"xtick.labelsize": 10})
+palette = "flare"
+# %% Config paths
+results_path = Path(__file__).parent / "results"
+figs_path = Path(__file__).parent / "figs"
+# %% Plot splits
+data_kind = "S4_R4"
+dfs = []
+for fname in results_path.glob(f"results_{data_kind}_*_splits_cv_scores.csv"):
+    t_df = pd.read_csv(fname)
+    n_splits = int(fname.stem.replace(f"results_{data_kind}", "").split("_")[1])
+    if n_splits == 0:
+        n_splits += 1
+    t_df["n_splits"] = n_splits
+    dfs.append(t_df)
+results_df = pd.concat(dfs)
+# %%
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+sns.swarmplot(
+    x="n_splits",
+    y="fit_time",
+    data=results_df,
+    hue="fold",
+    ax=axes[0],
+)
+
+sns.swarmplot(
+    x="n_splits",
+    y="test_neg_mean_absolute_error",
+    data=results_df,
+    hue="fold",
+    ax=axes[1],
+)
+fig.subplots_adjust(wspace=0.3)
+if data_kind == "S4_R4":
+    fig.suptitle(f"fastGPR for {data_kind} (29852 features)")
+else:
+    fig.suptitle(f"fastGPR for {data_kind} (PCA)")
+# %% Plot n_features + splits
+
+data_kind = "S4_R8"
+
+dfs = []
+for fname in results_path.glob(
+    f"results_{data_kind}_*_splits_*_features_cv_scores.csv"
+):
+    t_df = pd.read_csv(fname)
+    n_splits = int(fname.stem.replace(f"results_{data_kind}", "").split("_")[1])
+    if n_splits == 0:
+        n_splits += 1
+    t_df["n_splits"] = n_splits
+    dfs.append(t_df)
+results_df = pd.concat(dfs)
+
+
+stats_df = results_df.groupby(["n_features", "n_splits"])[
+    ["fit_time", "test_neg_mean_absolute_error"]
+].mean()
+stats_df.reset_index()
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+sns.swarmplot(
+    x="n_splits",
+    y="fit_time",
+    data=stats_df,
+    hue="n_features",
+    ax=axes[0],
+    palette=palette,
+)
+axes[0].legend(loc=1, prop={"size": 8}, title="# Features")
+axes[0].set_xlabel("# Splits")
+axes[0].set_ylabel("Fit time (s)")
+axes[0].set_xticklabels(["No Split", "2", "3", "4", "5"])
+sns.swarmplot(
+    x="n_splits",
+    y="test_neg_mean_absolute_error",
+    data=stats_df,
+    hue="n_features",
+    ax=axes[1],
+    palette=palette,
+    legend=False,
+)
+axes[1].set_xlabel("# Splits")
+axes[1].set_ylabel("-(Mean Absolute Error)")
+axes[1].set_xticklabels(["No split", "2", "3", "4", "5"])
+fig.subplots_adjust(wspace=0.2)
+
+fig.suptitle(f"fastGPR performance by number of features and splits")
+plt.savefig(
+    figs_path / f"fig_{data_kind}_features_splits.pdf",
+    bbox_inches="tight")
+stats_df.to_csv(figs_path / f"results_{data_kind}_features_splits.csv")
+# %% Plot n_repeats + splits
+
+data_kind = "S4_R8"
+
+dfs = []
+for fname in results_path.glob(f"results_{data_kind}_*_splits_*_repeats_cv_scores.csv"):
+    t_df = pd.read_csv(fname)
+    n_splits = int(fname.stem.replace(f"results_{data_kind}", "").split("_")[1])
+    if n_splits == 0:
+        n_splits += 1
+    t_df["n_splits"] = n_splits
+    dfs.append(t_df)
+results_df = pd.concat(dfs)
+
+baseline_df = pd.read_csv(results_path / f"results_{data_kind}_0_splits_cv_scores.csv")
+base_stats = baseline_df[
+    [
+        "fit_time",
+        "test_neg_mean_absolute_error",
+        "test_neg_mean_squared_error",
+        "test_r2",
+    ]
+].mean()
+
+stats_df = results_df.groupby(["n_repeats", "n_splits"])[
+    [
+        "fit_time",
+        "test_neg_mean_absolute_error",
+        "test_neg_mean_squared_error",
+        "test_r2",
+    ]
+].mean()
+stats_df.reset_index()
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+sns.swarmplot(
+    x="n_splits",
+    y="fit_time",
+    data=stats_df,
+    hue="n_repeats",
+    ax=axes[0],
+    legend="full",
+    palette=palette,
+)
+axes[0].axhline(
+    base_stats["fit_time"],
+    color="red",
+    linestyle="--",
+    label="No Split",
+)
+axes[0].legend(loc=1, prop={"size": 7}, title="# Repeats")
+axes[0].set_xlabel("# Splits")
+axes[0].set_ylabel("Fit time (s)")
+sns.swarmplot(
+    x="n_splits",
+    y="test_neg_mean_absolute_error",
+    data=stats_df,
+    hue="n_repeats",
+    ax=axes[1],
+    legend=False,
+    palette=palette,
+)
+axes[1].axhline(
+    base_stats["test_neg_mean_absolute_error"],
+    color="red",
+    linestyle="--",
+    label="baseline",
+)
+axes[1].set_xlabel("# Splits")
+axes[1].set_ylabel("-(Mean Absolute Error)")
+fig.subplots_adjust(wspace=0.2)
+
+fig.suptitle(f"fastGPR performance by number of splits and repeats")
+plt.savefig(
+    figs_path / f"fig_{data_kind}_repeats_splits.pdf",
+    bbox_inches="tight")
+stats_df.to_csv(figs_path / f"results_{data_kind}_repeats_splits.csv")
+base_stats.to_csv(figs_path / f"results_{data_kind}_GPR.csv")
+# %%
